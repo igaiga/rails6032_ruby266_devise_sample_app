be3b48f259e9d69d01b87fa45e77fd6e08f80082
Kuniaki Igarashi <igaiga@gmail.com>
2020-07-31 11:38:11 +0900

    Add test for members/index with sign_in helper

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class MembersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should get index" do
+    sign_in users(:one)
     get members_index_url
     assert_response :success
   end
-
 end
